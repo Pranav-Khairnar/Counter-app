7ece88368176e679d5fc47331ed895651c335dbd
Pranav-Khairnar <forcode.pranav@gmail.com>
2023-03-23 19:45:56 +0530

    First Commit

--- src/pages/counterpage/Counter.js
+++ src/pages/counterpage/Counter.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './counter.css'
+import Switch from "react-switch";
+import {styleLight,styleDark} from './style'
+
 export default function Counter() {
-    
+
     //states
+    const[styling, setStyle] = useState(styleLight)
+    const[mode, setMode] = useState(true)
     const[count, setCount] = useState(0);
+
+
     
+
     //buttons
     const increase = () => {
         setCount(count+1)
@@ -17,10 +25,23 @@ export default function Counter() {
     const reset = () => {
         setCount(0)
     }
+    const toggle = () => {
+        if(mode){
+            setStyle(styleDark);
+            setMode(false);
+        } else {
+            setStyle(styleLight);
+            setMode(true);
+        }
+    }
+
+    const hchange = () => {
+
+    }
     
 
     return (
-        <div className = "page">
+        <div /* className = "page" */  style={styling}>
             <div className="displayNumber">
                 <p>
                     {count}
@@ -30,7 +51,10 @@ export default function Counter() {
                 <button onClick={increase}>INCREASE</button>
                 <button onClick={decrease}>DECREASE</button>
                 <button onClick={reset}>RESET</button>
+                {/* <button onClick={toggle}>Toggle</button> */}
+                
             </div> 
+            <Switch onChange={toggle} checked = {mode} uncheckedIcon={false} checkedIcon={false} offColor="#f578d1" onColor='#2b6ced'/>
         </div>
     )
 }
---
+++ src/pages/counterpage/style.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const styleLight = {
+        color: 'red',
+        backgroundImage:'linear-Gradient(to right, rgb(255, 123, 213), rgb(198, 223, 254))',
+        width: '100%',
+        height:'100vh',
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        justifyContent:'center',
+        transition: 'all 2s'
+    }
+    
+    export const styleDark = {
+        color: 'red',
+        backgroundImage:'linear-Gradient(to right, rgb(0,0,0),rgb(0,0,0),rgb(0,0,0), rgb(44, 139, 255))',
+        width: '100%',
+        height:'100vh',
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        justifyContent:'center',
+        transition: 'all 2s 2s'
+        
+}
